7dc799938acbf603b27499d245ae841de6832596
Rhapsody0128 <rhapsody0128@gmail.com>
2020-09-14 21:20:24 +0800

    FB login 練習

---
+++ src/views/FB login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="FB_login">
+    <button @click="fblogin()">登入</button>
+    <!-- <button @click="logout()">登出</button> -->
+  </div>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    fblogin () {
+      window.FB.login(response => {
+        console.log(response)
+      })
+    }
+  },
+  mounted () {
+  }
+}
+</script>
