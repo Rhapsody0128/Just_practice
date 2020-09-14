9a95e3548eab82398aca1f7816e3fa4fc815c85c
Rhapsody0128 <rhapsody0128@gmail.com>
2020-09-14 23:32:12 +0800

    sdk

--- src/views/FB login.vue
+++ src/views/FB login.vue
@@ -19,6 +19,13 @@ export default {
     }
   },
   mounted () {
+    (function (d, s, id) {
+      var js; var fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) { return }
+      js = d.createElement(s); js.id = id
+      js.src = 'https://connect.facebook.net/en_US/sdk.js'
+      fjs.parentNode.insertBefore(js, fjs)
+    }(document, 'script', 'facebook-jssdk'))
   }
 }
 </script>
